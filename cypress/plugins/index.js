74cedbe85ea5d7bf83f79fd18301832276ab30b2
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-04-12 11:31:10 -0300

    using @cypress/react

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const injectDevServer = require("@cypress/react/plugins/react-scripts")
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -17,6 +18,6 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  injectDevServer(on, config)
+  return config
 }
--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,34 +1,12 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+/// <reference types="cypress" />
+import { mount } from '@cypress/react';
 import App from './App';
 
 describe('App Component', () => {
-  it('should render button', () => {
-    render(<App />);
-    expect(screen.getByTestId('add+1-button')).toBeInTheDocument();
-  });
-
-  it('should render list', () => {
-    render(<App />)
-    expect(screen.getByRole('list')).toBeInTheDocument();
-  })
-
-  it('should add on click', async () => {
-    const { rerender } = render(<App />)
-    const addButton = screen.getByTestId('add-item-button');
-    const input = screen.getByPlaceholderText('item');
-
-    userEvent.type(input, 'aaa')
-    userEvent.click(addButton);
-
-    await waitFor(() => {
-      expect(screen.getByText('aaa'))
-        .toBeInTheDocument();
-    }, {
-      interval: 1000,
-    });
-
-    // expect(await screen.findByText('aaa'))
-    //   .toBeInTheDocument();
+  it('should add on click', () => {
+    mount(<App />);
+    cy.get('[data-cy="text-input"]').type('aaa', { force: true });
+    cy.get('[data-cy="add-item-button"]').click();
+    cy.get('[data-cy="jojo"]').should('to.have.length', 1);
   })
 });
